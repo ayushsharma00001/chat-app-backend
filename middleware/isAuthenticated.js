f2342763f1f00cd623287771647cf2848a2ee1bf
Ayush Sharma <116947667+ayushsharma00001@users.noreply.github.com>
2024-07-30 17:12:14 +0530

    Update isAuthenticated.js 1

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken";
 const isAuthenticated = async (req,res,next)=>{
     try{
         const token = req.cookies.token;
+        console.log("Token:",token);
         if(!token){
             res.status(401).json({message:"user not authenticated"})
         };
